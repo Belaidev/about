8eec00c7818ee60bd5a41182b88a465100dd9965
Belaidev <belaidev@protonmail.com>
2023-11-13 17:34:20 -0500

    Set up project

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <p>
+      <span>
+        I'm <b>Riad Belaid.</b>
+      </span>
+      <br />
+      <span>
+        I'm a <b>full-stack developer</b>
+      </span>
+      <br />
+      <span>Please, take a look around.</span>
+    </p>
+  );
+}
