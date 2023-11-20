ebb688ea4bf476e1b110a7e02dd716e9cc87a9bc
Belaidev <belaidev@protonmail.com>
2023-11-19 23:21:03 -0500

    Modified project configuration

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+export default {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:@typescript-eslint/stylistic',
+		'plugin:solid/typescript'
+	],
+	overrides: [
+		{
+			env: {
+				node: true
+			},
+			files: ['.eslintrc.js'],
+			parserOptions: {
+				sourceType: 'script'
+			}
+		}
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json'
+	},
+	plugins: ['@typescript-eslint', 'solid'],
+	rules: {
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-confusing-void-expression': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '_' }],
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs']
+	}
+};
